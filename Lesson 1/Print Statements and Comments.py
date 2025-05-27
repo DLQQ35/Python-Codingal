8e04fa77525c17c3c997cde85e09eccc51fc5ad5
DLQQ35 <architkhanna260912@gmail.com>
2025-05-27 21:18:18 +0530

    Lesson 1

---
+++ Lesson 1/Print Statements and Comments.py
@@ -0,0 +1,8 @@
+#printing the greeting message
+print("Welcome")
+#printing my name
+print("My name is Archit")
+#printing a fun fact about myself
+print("I love to solve Rubik's Cube")
+#adding two numbers
+print("Sum of 4+7 is", 4+7)
