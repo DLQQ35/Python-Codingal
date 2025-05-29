27a8b63dfccebe2939e65a1eb1790d88396de9e4
DLQQ35 <architkhanna260912@gmail.com>
2025-05-29 21:20:58 +0530

    After-Class Project

---
+++ After-Class Projects/Birthday.py
@@ -0,0 +1,7 @@
+ansh = "01/08/12"
+aaradhay = "19/11/12"
+archit = "26/09/12"
+
+print("Ansh's birthday is on", ansh)
+print("Aaradhay's birthday is on", aaradhay)
+print("Archit's birthday is on", archit)
