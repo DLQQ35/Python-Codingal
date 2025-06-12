758705ad1248e74a6df1f01f3edda801c74a7b38
DLQQ35 <architkhanna260912@gmail.com>
2025-06-12 20:43:57 +0530

    Lesson 6

---
+++ Lesson 6/Not equal.py
@@ -0,0 +1,33 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a,"and",b,"are different")
+else:
+    print(a,"and",b,"are same")
+
+
+a = 4
+b = 5
+
+if (a == 1) != (b ==5):
+    print(a,"=1 is equal to",b,"=5 is false")
+
+
+a = int(input("Enter a number:"))
+
+if a%2 != 0:
+    print(a,"is an odd number")
+else:
+    print(a,"is an even number")
+
+
+
