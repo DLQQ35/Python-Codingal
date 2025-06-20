9bcb031a065d55f172ffb9e967efd37621940bb4
DLQQ35 <architkhanna260912@gmail.com>
2025-06-20 21:14:42 +0530

    After-Class Projects

---
+++ After-Class Projects/Square Root.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+import math
+
+ANS = math.sqrt(num)
+print(ANS)
---
+++ After-Class Projects/Summer Times.py
@@ -0,0 +1,6 @@
+temp = float(input("Enter the temperature in degrees: "))
+
+if temp > 18.00:
+    print("It is hot outside")
+else:
+    print("It is cold outside")
