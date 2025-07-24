785b5a76ee49db5c4c61a10bad20027f189549f9
DLQQ35 <architkhanna260912@gmail.com>
2025-07-24 21:08:47 +0530

    After-Class Project

---
+++ After-Class Projects/Trignometry.py
@@ -0,0 +1,7 @@
+import math
+
+num = int(input("Enter a number: "))
+
+print("The sin of",num,"is",math.sin(num))
+print("The cos of",num,"is",math.cos(num))
+print("The tan of",num,"is",math.tan(num))
