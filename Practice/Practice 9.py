d357e4c6f5d4d35749570e40f7f90322fccb5f0c
DLQQ35 <architkhanna260912@gmail.com>
2025-09-04 21:17:37 +0530

    Practice

--- Practice/Practice 9.py
+++ Practice/Practice 9.py
@@ -86,5 +86,141 @@ while True:
         t.width(275)
     elif w == "300":
         t.width(300)
-    elif w == "":
-        t.width()
+    elif w == "325":
+        t.width(325)
+    elif w == "350":
+        t.width(350)
+    elif w == "375":
+        t.width(375)
+    elif w == "400":
+        t.width(400)
+    elif w == "425":
+        t.width(425)
+    elif w == "450":
+        t.width(450)
+    elif w == "475":
+        t.width(475)
+    elif w == "500":
+        t.width(500)
+    elif w == "525":
+        t.width(525)
+    elif w == "550":
+        t.width(550)
+    elif w == "575":
+        t.width(575)
+    elif w == "600":
+        t.width(600)
+    elif w == "625":
+        t.width(625)
+    elif w == "650":
+        t.width(650)
+    c = input("Select Pen Color:")
+    if c == "red":
+        t.pencolor("red")
+    elif c == "blue":
+        t.pencolor("blue")
+    elif c == "green":
+        t.pencolor("green")
+    elif c == "black":
+        t.pencolor("black")
+    elif c == "white":
+        t.pencolor("white")
+    elif c == "yellow":
+        t.pencolor("yellow")
+    elif c == "light yellow":
+        t.pencolor("light yellow")
+    elif c == "light blue":
+        t.pencolor("light blue")
+    elif c == "light red":
+        t.pencolor("light red")
+    elif c == "light green":
+        t.pencolor("light green")
+    elif c == "cyan":
+        t.pencolor("cyan")
+    elif c == "grey":
+        t.pencolor("grey")
+    elif c == "brown":
+        t.pencolor("brown")
+    elif c == "magenta":
+        t.pencolor("magenta")
+    elif c == "orange":
+        t.pencolor("orange")
+    elif c == "pink":
+        t.pencolor("pink")
+    elif c == "purple":
+        t.pencolor("purple")
+    elif c == "violet":
+        t.pencolor("violet")
+    elif c == "indigo":
+        t.pencolor("indigo")
+    elif c == "turquoise":
+        t.pencolor("turquoise")
+    elif c == "lime":
+        t.pencolor("lime")
+    elif c == "aqua":
+        t.pencolor("aqua")
+    elif c == "maroon":
+        t.pencolor("maroon")
+    elif c == "gold":
+        t.pencolor("gold")
+    d = input("Select the direction and steps taken by the turtle:")
+    if d == "f1":
+        t.fd(1)
+    elif d == "f10":
+        t.fd(10)
+    elif d == "f20":
+        t.fd(20)
+    elif d == "f30":
+        t.fd(30)
+    elif d == "f40":
+        t.fd(40)
+    elif d == "f50":
+        t.fd(50)
+    elif d == "f60":
+        t.fd(60)
+    elif d == "f70":
+        t.fd(70)
+    elif d == "f80":
+        t.fd(80)
+    elif d == "f90":
+        t.fd(90)
+    elif d == "f100":
+        t.fd(100)
+    elif d == "b1":
+        t.bd(1)
+    elif d == "b10":
+        t.bd(10)
+    elif d == "b20":
+        t.bd(20)
+    elif d == "b30":
+        t.bd(30)
+    elif d == "b40":
+        t.bd(40)
+    elif d == "b50":
+        t.bd(50)
+    elif d == "b60":
+        t.bd(60)
+    elif d == "b70":
+        t.bd(70)
+    elif d == "b80":
+        t.bd(80)
+    elif d == "b90":
+        t.bd(90)
+    elif d == "b100":
+        t.bd(100)
+    elif d == "r45":
+        t.rt(45)
+    elif d == "r90":
+        t.rt(90)
+    elif d == "r135":
+        t.rt(135)
+    elif d == "r180":
+        t.rt(180)
+    elif d == "r225":
+        t.rt(225)
+    elif d == "r270":
+        t.rt(270)
+    elif d == "r315":
+        t.rt(315)
+    elif d == "r360":
+        t.rt(360)
