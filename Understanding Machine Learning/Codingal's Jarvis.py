3fec08325f729183ffc03269fa41a840228b269c
DLQQ35 <architkhanna260912@gmail.com>
2026-02-06 21:03:19 +0530

    Understanding Machine Learning

--- Understanding Machine Learning/Codingal's Jarvis.py
+++ Understanding Machine Learning/Codingal's Jarvis.py
@@ -30,11 +30,11 @@ pairs = [
         ["Hello", "Hey there",]
     ],
     [
-        r"What is your name ?",
+        r"What is your name \?",
         ["I am a chatbot created by Codingal. You can call me Jarvis.",]
     ],
     [
-        r"How are you ?",
+        r"How are you \?",
         ["I'm doing good. How about you ?",]
     ],
     [
@@ -50,27 +50,27 @@ pairs = [
         ["Great to hear that. How can I help you?",]
     ],
     [
-        r"(*) age?",
+        r"(.*) age\?",
         ["I'm a computer program dude, so I don't have an age.",]
     ],
     [
-        r"What (.*) want ?",
+        r"What (.*) want \?",
         ["Make me an offer I can't refuse",]
     ],
     [
-        r"(.*) created ?",
+        r"(.*) created \?",
         ["I was created by Codingal.","TOP SECRET ;)",]
     ],
     [
-        r"(.*) (location|city) ?",
+        r"(.*) (location|city) \?",
         ['I am based on the cloud.',]
     ],
     [
-        r"How is the weather in (.*)?",
+        r"How is the weather in (.*)\?",
         ["Weather in %1 is awesome like always", "Too hot here in %1", "Too cold here in %1", "Never even heard about %1"]
     ],
     [
-        r"I work in (.*)?",
+        r"I work in (.*)\?",
         ["%1 is an amazing company, I have heard about it.", "I have heard about %1, but I don't know much about it.", "I don't know anything about %1."]
     ],
     [
@@ -78,23 +78,23 @@ pairs = [
         ["No rain in %2", "Damn it's raining in %2", "It's perfect weather in %2"]
     ],
     [
-        r"How (.*) health(.*)?",
+        r"How (.*) health(.*)\?",
         ["Health is very important, but I am a computer program, so I don't have health.", "I'm a computer program dude, SERIOUSLY.","You are making me feel the FOMO",]
     ],
     [
-        r"(.*)(sports|game|sportsperson|athlete)?",
+        r"(.*)(sports|game|sportsperson|athlete)\?",
         ["I'm a very big fan of Football", "I'm a very big fan of Cricket", "I'm a very big fan of Basketball"]
     ],
     [
-        r"Who (.*) sportsperson ?",
+        r"Who (.*) sportsperson \?",
         ["Messi", "Ronaldo", "Neymar"]
     ],
     [
-        r"Who (.*) (moviestar|actor)?",
+        r"Who (.*) (moviestar|actor)\?",
         ["Brad Pitt", "Leonardo DiCaprio", "Tom Cruise"]
     ],
     [
-        r"I am looking for online guides and courses to learn data science, can you suggest?",
+        r"I am looking for online guides and courses to learn data science, can you suggest\?",
         ["Jarvis_Tech has many great articles with each step explanation along with code, you can explore"]
     ],
     [
